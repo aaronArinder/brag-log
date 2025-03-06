609662af5f57ec2f79ff859b20a8846057e2adb8
Aaron Arinder <aaronarinder@gmail.com>
2025-03-06 12:52:58 -0500

    feat: add ladder cmd, add opt for bucket

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use chrono::Utc;
-use clap::{Arg, ArgAction, Command};
+use clap::{arg, Arg, ArgAction, Command};
 use std::io::{Read, Write};
 use std::process::exit;
 use std::{env, fs::OpenOptions, path::PathBuf};
@@ -14,10 +14,16 @@ fn main() {
     std::fs::create_dir_all(brag_log_dir.clone()).expect("failed to create brag directory");
 
     let brag_log = brag_log_dir.join("log");
+    let ladder = brag_log_dir.join("ladder");
 
     let cmd = clap::Command::new("brag")
-        .subcommand(Command::new("add").arg(Arg::new("log").action(ArgAction::Set)))
-        .subcommand(Command::new("read"));
+        .subcommand(Command::new("add")
+            .args([
+                arg!(-b --bucket <BUCKET> "Tag the appropriate career ladder bucket for this brag to fall under").required(true), 
+                Arg::new("log").action(ArgAction::Set).required(true)
+            ]))
+        .subcommand(Command::new("read"))
+        .subcommand(Command::new("ladder"));
 
     let matches = cmd.get_matches();
 
@@ -27,11 +33,17 @@ fn main() {
                 .append(true)
                 .create(true)
                 .open(brag_log)
-                .expect("failed to open brag log at runtime");
+                .expect("failed to open brag log");
 
             let today = Utc::now().format("%Y-%m-%d");
 
-            writeln!(file, "{today} | {input}").expect("failed to write to brag log");
+            let ladder_bucket = cmd
+                .get_one::<String>("bucket")
+                .expect("failed to get required flag");
+
+            writeln!(file, "{today} | {ladder_bucket} |{input}")
+                .expect("failed to write to brag log");
+
             exit(0)
         }
     }
@@ -49,5 +61,21 @@ fn main() {
         println!("{content}");
         exit(0)
     }
+
+    if let Some(_cmd) = matches.subcommand_matches("ladder") {
+        let mut ladder = OpenOptions::new()
+            .read(true)
+            .open(ladder)
+            .expect("failed to read ladder document");
+
+        let mut content = String::new();
+        ladder
+            .read_to_string(&mut content)
+            .expect("failed to read ladder to string");
+
+        println!("{content}");
+        exit(0)
+    }
+
     exit(1)
 }
